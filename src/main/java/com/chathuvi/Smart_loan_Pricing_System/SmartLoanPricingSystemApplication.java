ae39ccda50cce4f48096fd8eb0b381e245f3b320
Chathuvi Godage <rasoja.20220072@iit.ac.lk>
2025-12-19 16:02:30 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/chathuvi/Smart_loan_Pricing_System/SmartLoanPricingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.chathuvi.Smart_loan_Pricing_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartLoanPricingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartLoanPricingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chathuvi/Smart_loan_Pricing_System/SmartLoanPricingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chathuvi.Smart_loan_Pricing_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartLoanPricingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
